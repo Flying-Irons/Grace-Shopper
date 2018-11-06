37916015ba1adf7b2468afc6fd38e1472873fff4
Adam Zhou <adzhou218@gmail.com>
2018-11-06 17:11:22 -0500

    cart exists to user

--- client/store/cart_store.js
+++ client/store/cart_store.js
@@ -225,7 +225,7 @@ export const decrementQuantityThunk = (productId, cartId, quantity) => {
 const CartReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_CART_PRODUCTS:
-      return {...state, products: action.products}
+      return {...state, products: [...action.products]}
     case ADD_TO_CART:
       return {...state, products: [...state.products, action.product]}
     case UPDATE_CART:
